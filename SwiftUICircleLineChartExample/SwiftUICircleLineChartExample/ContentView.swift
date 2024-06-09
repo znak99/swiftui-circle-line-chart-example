1a863802775c40de1d81756fef6cf0ed8df8c37e
znak99 <xhahekcldhkznak0113@gmail.com>
2024-06-09 19:19:32 +0900

    Initial commit

---
+++ SwiftUICircleLineChartExample/SwiftUICircleLineChartExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUICircleLineChartExample
+//
+//  Created by seungwoo on 2024/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICircleLineChartExample/SwiftUICircleLineChartExample/SwiftUICircleLineChartExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICircleLineChartExampleApp.swift
+//  SwiftUICircleLineChartExample
+//
+//  Created by seungwoo on 2024/06/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICircleLineChartExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
